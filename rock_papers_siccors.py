48b0fe340162f8b05ee385fda5ecfac095803148
Rayyan Abdullah Khan <rayyanabdullah1002@gmail.com>
2025-10-05 16:17:56 +0500

    Fixed The Error message system
     - Program would stop if number was invalid but now it repeatedly       ask the user for it.
    
    - Will make programs with functions rather basic ifs and buts in the future.

--- rock_papers_siccors.py
+++ rock_papers_siccors.py
@@ -9,20 +9,27 @@ print("3) ✌️")
 while True:
     player = int(input('\nEnter a number!'))
     if player < 1 or player > 3:
-        print('Error enter only from 1 to 3 inclusive')
-    break
+        print('\n --  |E R R O R|  enter only from 1 to 3 inclusive -- ')
+    else:
+       break
 
 import random
 computer = random.randint(1, 3)
 
 choices = {1: '✊', 2: '✋', 3: '✌️'}
 
-print(f'\n you chose {choices[player]}')
-print(f'computer chose {choices[computer]}')
+print(f'\n you chose      :{choices[player]}')
+print(f'\n computer chose :{choices[computer]}')
 
 if player == computer:
+    print('='*40)
     print('-- Its a tie! --')
+    print('='*40)
 elif (player == 1 and computer == 3) or (player == 2 and computer == 1) or (player == 3 and computer == 2):
+    print('='*40)
     print('-- Player won!! --')
+    print('='*40)
 else:
+    print('='*40)
     print('-- computer won!! --')
+    print('='*40)
